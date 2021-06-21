d089fd6bdeea4ffa51a8119ae5bc530494f13eb6
mawencan <1819293933@qq.com>
2021-06-22 00:30:30 +0800

    feat: 随时间变化怪兽类型

--- src/pixi/npc/fish.js
+++ src/pixi/npc/fish.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js'
 import { Fish } from '../lib/fish'
+import { gameTime } from '../reactivity'
 import createNpc from './type'
 let time
 export function factoryFishPause () {
@@ -11,20 +12,24 @@ export function factoryFish (app, organization) {
       if (Math.random() > 0.5) {
         createFish(app, organization)
         createFish(app, organization)
+        createFish(app, organization)
       } else {
         createFish(app, organization)
       }
-  }, 2500)
+  }, 3000)
 }
 export async function createFish (app, organization) {
   const {
     texture, 
     healthValue
-  } = await createNpc(0)
+  } = await createNpc(createType())
   const sprite = new PIXI.Sprite(texture)
   const fish = new Fish(app, sprite, Math.random() > 0.5 ? 'r' : 'l', healthValue)
   fish.positionOut()
   organization
     .addMaterial(fish)
   fish.startMove()
+}
+export function createType () {
+  return Math.floor(gameTime.value / 10)
 }
--- src/pixi/npc/type.js
+++ src/pixi/npc/type.js
@@ -3,16 +3,61 @@ export default function createNpc (type) {
   switch (type) {
     case 0:
       return ranYiYu()
+    case 1:
+      return guDiao()
+    case 2:
+      return gyarados()
+    case 3:
+      return qiongQi()
+    case 4:
+      return xingTian()
     default:
       return ranYiYu()
   }
 }
 // 冉遗鱼
 export async function ranYiYu () {
-  const texture = await getTexture('tilapiaFish', require('../../assets/ran-yi-yu.png'))
+  const texture = await getTexture('../../assets/ran-yi-yu.png', require('../../assets/ran-yi-yu.png'))
   return {
     texture,
     healthValue: 50,
     delicious: 1
   }
 }
+// 蛊雕
+export async function guDiao () {
+  const texture = await getTexture('../../assets/gu-diao.png', require('../../assets/gu-diao.png'))
+  return {
+    texture,
+    healthValue: 80,
+    delicious: 2
+  }
+}
+// 暴鲤龙
+export async function gyarados () {
+  const texture = await getTexture('../../assets/gyarados.png', require('../../assets/gyarados.png'))
+  return {
+    texture,
+    healthValue: 120,
+    delicious: 3
+  }
+}
+// 穷奇
+export async function qiongQi () {
+  const texture = await getTexture('../../assets/qiong-qi.png', require('../../assets/qiong-qi.png'))
+  return {
+    texture,
+    healthValue: 180,
+    delicious: 4
+  }
+}
+// 刑天
+export async function xingTian () {
+  const texture = await getTexture('../../assets/xing-tian.png', require('../../assets/xing-tian.png'))
+  return {
+    texture,
+    healthValue: 270,
+    delicious: 5
+  }
+}
+
