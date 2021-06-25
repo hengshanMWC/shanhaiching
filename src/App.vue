6fb17417b29193dee7b2f9870beee454270e82e6
mawencan <mawencan@xinli001.com>
2021-06-25 18:35:28 +0800

    chore: 修改了.eslintrc规则

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,26 @@
     <div ref="pixiContainer" id="pixi"></div>
     <div class="top-right-box">
       <div class="game-status">
-        <div class="game-time">{{gameTime}}</div>
-        <div class="game-value">积分：{{gameValue}}</div>
+        <div class="game-time">{{ gameTime }}</div>
+        <div class="game-value">积分：{{ gameValue }}</div>
       </div>
       <audio ref="bgm">
-        <source src="./assets/audio/bgm.mp3" type="audio/mpeg" loop autoplay>
+        <source src="./assets/audio/bgm.mp3" type="audio/mpeg" loop autoplay />
       </audio>
-      <i v-if="isPlay" @click="isPlay = false" class="iconfont icon-bofangyinle"></i>
+      <i
+        v-if="isPlay"
+        @click="isPlay = false"
+        class="iconfont icon-bofangyinle"
+      ></i>
       <i v-else @click="isPlay = true" class="iconfont icon-guanbiyinle"></i>
       <i class="iconfont icon-a-shezhi"></i>
     </div>
     <div v-if="isIdle || isPause" class="center-box">
       <p v-if="isPlayed" class="end-text">{{ endText }}</p>
       <div v-if="isIdle" @click="handleStart" class="start-btn">山海经</div>
-      <div v-else-if="isPause" @click="handleContinue" class="start-btn">继续</div>
+      <div v-else-if="isPause" @click="handleContinue" class="start-btn">
+        继续
+      </div>
     </div>
   </div>
 </template>
@@ -25,19 +31,19 @@
 import { ref, computed, onMounted, watch } from 'vue'
 import { getGame } from './pixi'
 import {
-    isInit,
-    isIdle,
-    isPause,
-    isSuccess,
-    isPlayed,
-    gameTime,
-    gameValue
+  isInit,
+  isIdle,
+  isPause,
+  isSuccess,
+  isPlayed,
+  gameTime,
+  gameValue,
 } from './pixi/reactivity'
 export default {
-  setup () {
+  setup() {
     const isPlay = ref(false)
     const bgm = ref(null)
-    watch(isPlay, value => {
+    watch(isPlay, (value) => {
       if (value) {
         bgm.value.play()
       } else {
@@ -46,16 +52,15 @@ export default {
     })
     let game
     const pixiContainer = ref(null)
-    function handleStart () {
+    function handleStart() {
       isIdle.value = false
       isInit.value = false
     }
-    function handleContinue () {
+    function handleContinue() {
       isPause.value = false
     }
-    const endText = computed(() => isSuccess.value ? 
-      '恭喜你！羽化成鲲！' 
-      : '游戏结束，鲸落'
+    const endText = computed(() =>
+      isSuccess.value ? '恭喜你！羽化成鲲！' : '游戏结束，鲸落'
     )
     onMounted(async () => {
       game = getGame()
@@ -74,9 +79,9 @@ export default {
       gameValue,
       endText,
       isPlay,
-      bgm
+      bgm,
     }
-  }
+  },
 }
 </script>
 
@@ -90,7 +95,6 @@ html {
   overflow: hidden;
 }
 #app {
-  
 }
 </style>
 <style scoped>
@@ -128,7 +132,7 @@ html {
   font-size: 24px;
   cursor: pointer;
 }
-.top-right-box > *{
+.top-right-box > * {
   margin-right: 15px;
 }
 .game-time,
